00e2d8bb6a9cde66086f49c28003c45f2b8e7d30
clarajose2001 <144613620+clarajose2001@users.noreply.github.com>
2024-09-30 19:13:45 +0530

    Update settings.py

--- HouseRent/settings.py
+++ HouseRent/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-y*meut6@oot07ef92vd3$+)(g@%6bqb#t53$$p3y4r^=&w4+aa
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
